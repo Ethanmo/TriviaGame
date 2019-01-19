91f1030cd5ef035d61f6509f9de2c58cc0940ecc
Ethanmo <mythface00@gmail.com>
2019-01-18 16:47:51 -0800

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -47,37 +47,101 @@ var questionList = [
 var correctCount;
 var incorrectCount;
 var unansweredCount;
-var timer = 0;
+var timer = 30;
 var intervalId;
 var timeOutId;
+var resultScreenTimeOut;
+var questionIndex = -1;
 
 function reset(){
     correctCount = 0;
     incorrectCount = 0;
     unansweredCount = 0;
-
+    questionIndex = -1;
+    newQuestion();
 }
 
 function newQuestion() {
-    timer = 0;
-    runTimer();
-    timeOutId = setTimeout(timeOutScreen, 1000 * 5)    
+    if (questionIndex < questionList.length){
+        questionIndex++;
+        console.log("questionIndex++");
+        timer = 30;
+        runTimer();
+        timeOutId = setTimeout(timeOutScreen, 1000 * 30)
+        $(".questionArea").html(questionList[questionIndex].q);
+        $(".answerArea").empty();
+        
+        //display all the answer options
+        var answers = questionList[questionIndex].answerList;
+        for (var i = 0; i < answers.length; i++){
+            var answerDiv = $("<div>");
+            answerDiv.text(answers[i]);
+            answerDiv.attr("class", "answerOptions");
+            answerDiv.attr("id", i);
+            $(".answerArea").append(answerDiv);
+        }
+
+      
+
+        
+    } else {
+        showResult();
+    }
+}
+  
+
+    
+
+    $(document).on("click", ".answerOptions", function(){
+            clearInterval(intervalId);
+            var answerId = $(this).attr("id");
+            var correctId = questionList[questionIndex].correctIndex;
+            if (answerId === correctId){
+                console.log(correctId);
+                correctScreen();
+            } else {
+                incorrectScreen();
+            }
+        });
+
+//correct screen
+function correctScreen(){
+    //something
+    $(".questionArea").html("<h2>Correct</h2>");
+    showCorrectAnswer();
 }
 
+function incorrectScreen(){
+    $(".questionArea").html("<h2>Wrong</h2>");
+    showCorrectAnswer();
+}
+
+function showCorrectAnswer(){
+    $(".answerArea").html(questionList[questionIndex].answerList[questionList[questionIndex].correctIndex]);
+    clearTimeout(timeOutId);
+    console.log("new game in 3");
+    setTimeout(newQuestion, 3000);
+}
+
+//add 1 to timer
 function timeCount () {
-    timer++;
-    console.log(timer);
+    $(".timerArea").html("remaining time : " + timer);
+    timer--;
+    //console.log(timer);
 }
 
+//run function timeCount every second using setInterval
 function runTimer(){
+    console.log("timer is running")
     intervalId = setInterval(timeCount, 1000); 
 }
 
 function timeOutScreen(){
     //something
-    console.log("time up")
-    clearTimeout(timeOutId);
+    console.log("time up");
+    //add this when choosing correct or incorrect answers, no need for timeoutScreen
+    //clearTimeout(timeOutId); 
     clearInterval(intervalId);
 }
 
-newQuestion();
+reset();
